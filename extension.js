bdbc3c82825af4f653272c0091018a294bc414ae
mariotosdig <mariotosdig2@gmail.com>
2017-12-19 20:01:17 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -105,9 +105,9 @@
       songstats: true,
       commandLiteral: "!",
       blacklists: {
-        NSFW: "https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json",
-        OP: "https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json",
-        BANNED: "https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json"
+        NSFW: "https://rawgit.com/mariotosdig/custom/master/blacklists/NSFWlist.json",
+        OP: "https://rawgit.com/mariotosdig/custom/master/blacklists/OPlist.json",
+        BANNED: "https://rawgit.com/mariotosdig/custom/master/blacklists/BANNEDlist.json"
       }
     }));
 
